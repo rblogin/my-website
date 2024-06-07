d9e1abd92e491eb75b329a6bd5ee253043df23f0
rblogin <ritamf9@gmail.com>
2024-06-07 12:02:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('myForm').addEventListener('submit', function(e) {
   const time = document.getElementById('time').value;
 
   // Send data to Google Apps Script function using fetch API
-  fetch('/your-script-url', {
+  fetch('https://script.google.com/macros/s/AKfycbzKx2dG48J3el04L9ZHRO5Q50tYy4XgueoFaL-KaemElEaPS5WmKOZsyg2OH7zdwmST/exec', {
     method: 'POST',
     body: JSON.stringify({ name, date, time }),
   })
